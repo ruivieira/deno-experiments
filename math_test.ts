ca84591da70343a6ab5a041229cdabc11c4d3995
Rui Vieira <ruidevieira@googlemail.com>
2020-05-23 22:49:17 +0100

    Add preliminary tests

---
+++ math_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+import * as math from "./math.ts";
+
+Deno.test("Correct range size", () => {
+    let r = math.range(10);
+    assertEquals(r.length, 10);
+  });
+
+  Deno.test("Correct rep size", () => {
+    let r = math.rep([10, 10], 0);
+    assertEquals(r.length, 10);
+    assertEquals(r[0].length, 10);
+  });
