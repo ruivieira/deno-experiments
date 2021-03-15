b9033e0757341787be296f1d260b56ab1894d3aa
Rui Vieira <ruidevieira@googlemail.com>
2021-03-15 21:08:33 +0000

    Add PMML

--- misc/secrets.ts
+++ misc/secrets.ts
@@ -1,12 +1,13 @@
 /**
  * INFO: simple `.config` reader
  */
-export function readSecrets<T>(topic: string, file:string = "credentials.json"): T {
-    const HOME = Deno.env.get("HOME");
-    const text = Deno.readTextFileSync(
-        `${HOME}/.config/${topic}/${file}`
-    );
-    return JSON.parse(text)
+export function readSecrets<T>(
+  topic: string,
+  file: string = "credentials.json",
+): T {
+  const HOME = Deno.env.get("HOME");
+  const text = Deno.readTextFileSync(
+    `${HOME}/.config/${topic}/${file}`,
+  );
+  return JSON.parse(text);
 }
-
-
