6743069a26c1daba50ae529fec438387ec582e91
Rui Vieira <ruidevieira@googlemail.com>
2021-03-15 21:10:01 +0000

    Misc

--- misc/test_st.ts
+++ misc/test_st.ts
@@ -1,10 +1,10 @@
-import {Syncthing} from "../syncthing/core.ts"
-import {readSecrets} from "./secrets.ts"
+import { Syncthing } from "../syncthing/core.ts";
+import { readSecrets } from "./secrets.ts";
 
-const credentials = readSecrets<any>("syncthing")
+const credentials = readSecrets<any>("syncthing");
 
-const st = new Syncthing(credentials.apiKey, "localhost")
+const st = new Syncthing(credentials.apiKey, "localhost");
 
 //With Callback
-st.system.ping().then(r => console.log(r))
-st.database.completion().then(r => console.log(r))
+st.system.ping().then((r) => console.log(r));
+st.database.completion().then((r) => console.log(r));
