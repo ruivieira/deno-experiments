21c81ebeed9c86d4a0f34aa0753af6f3020ee8f1
Rui Vieira <ruidevieira@googlemail.com>
2021-03-16 22:12:17 +0000

    Format for CI

--- misc/matrices.ts
+++ misc/matrices.ts
@@ -1,7 +1,7 @@
-import {Matrix} from "../mentat/linalg/core.ts";
+import { Matrix } from "../mentat/linalg/core.ts";
 
-const m = new Matrix([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3)
-console.log(m.toString(1))
+const m = new Matrix([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3);
+console.log(m.toString(1));
 
-m.addColumn([0.5, 0.5, 0.5])
-console.log(m.toString(1))
+m.addColumn([0.5, 0.5, 0.5]);
+console.log(m.toString(1));
--- misc/regression.ts
+++ misc/regression.ts
@@ -1,17 +1,17 @@
-import {MLR} from '../external/regression.js'
+import { MLR } from "../external/regression.js";
 
 const x = [
-    [0, 0],
-    [1, 2],
-    [2, 3],
-    [3, 4]
+  [0, 0],
+  [1, 2],
+  [2, 3],
+  [3, 4],
 ];
 // Y0 = X0 * 2, Y1 = X1 * 2, Y2 = X0 + X1
 const y = [
-    [0, 0, 0],
-    [2, 4, 3],
-    [4, 6, 5],
-    [6, 8, 7]
+  [0, 0, 0],
+  [2, 4, 3],
+  [4, 6, 5],
+  [6, 8, 7],
 ];
 const mlr = new MLR(x, y);
 console.log(mlr.predict([3, 3]));
