61b02bd26217cd62ae728efbd1808fe9ab74f801
Rui Vieira <ruidevieira@googlemail.com>
2021-03-16 09:15:24 +0000

    Add fluent DSL for containers

--- containers/core.ts
+++ containers/core.ts
@@ -1,166 +1,231 @@
 type Environment = { [p: string]: string };
 
 interface Entry {
-  render(): string;
+    render(): string;
 }
 
 export class Base implements Entry {
-  public readonly from: string;
-  public readonly tag: string;
+    public readonly from: string;
+    public readonly tag: string;
 
-  constructor(from: string, tag: string = "latest") {
-    this.from = from;
-    this.tag = tag;
-  }
+    constructor(from: string, tag: string = "latest") {
+        this.from = from;
+        this.tag = tag;
+    }
 
-  render(): string {
-    return `FROM ${this.from}:${this.tag}`;
-  }
+    render(): string {
+        return `FROM ${this.from}:${this.tag}`;
+    }
 }
 
 export class Command implements Entry {
-  private commands: string[] = [];
-  constructor() {}
-  add(cmd: string): Command {
-    this.commands.push(cmd);
-    return this;
-  }
+    private commands: string[] = [];
 
-  render(): string {
-    const c = this.commands.map((c) => `"${c}"`).join(", ");
-    return `CMD [${c}]`;
-  }
+    constructor() {
+    }
+
+    add(cmd: string): Command {
+        this.commands.push(cmd);
+        return this;
+    }
+
+    render(): string {
+        const c = this.commands.map((c) => `"${c}"`).join(", ");
+        return `CMD [${c}]`;
+    }
 }
 
 export class Env implements Entry {
-  private envs: Environment = {};
-  constructor() {}
-  add(key: string, value: string): Env {
-    this.envs[key] = value;
-    return this;
-  }
-  render(): string {
-    const result = [];
-    for (const key of Object.keys(this.envs)) {
-      result.push(`ENV ${key}=${this.envs[key]}`);
+    private envs: Environment = {};
+
+    constructor() {
     }
-    return result.join("\n");
-  }
+
+    add(key: string, value: string): Env {
+        this.envs[key] = value;
+        return this;
+    }
+
+    render(): string {
+        const result = [];
+        for (const key of Object.keys(this.envs)) {
+            result.push(`ENV ${key}=${this.envs[key]}`);
+        }
+        return result.join("\n");
+    }
 }
 
 export class Port implements Entry {
-  public readonly port: number;
-  constructor(port: number) {
-    this.port = port;
-  }
-  render(): string {
-    return `EXPOSE ${this.port}`;
-  }
+    public readonly port: number;
+
+    constructor(port: number) {
+        this.port = port;
+    }
+
+    render(): string {
+        return `EXPOSE ${this.port}`;
+    }
 }
 
 export class Workdir implements Entry {
-  private readonly dir: string;
-  constructor(dir: string) {
-    this.dir = dir;
-  }
-  render(): string {
-    return `WORKDIR ${this.dir}`;
-  }
+    private readonly dir: string;
+
+    constructor(dir: string) {
+        this.dir = dir;
+    }
+
+    render(): string {
+        return `WORKDIR ${this.dir}`;
+    }
 }
 
 export class User implements Entry {
-  private readonly user: string;
-  constructor(user: string) {
-    this.user = user;
-  }
-  render(): string {
-    return `USER ${this.user}`;
-  }
+    private readonly user: string;
+
+    constructor(user: string) {
+        this.user = user;
+    }
+
+    render(): string {
+        return `USER ${this.user}`;
+    }
 }
 
 export class Copy implements Entry {
-  private readonly source: string;
-  private readonly dest: string;
-  constructor(source: string, dest: string) {
-    this.source = source;
-    this.dest = dest;
-  }
-  render(): string {
-    return `COPY ${this.source} ${this.dest}`;
-  }
+    private readonly source: string;
+    private readonly dest: string;
+
+    constructor(source: string, dest: string) {
+        this.source = source;
+        this.dest = dest;
+    }
+
+    render(): string {
+        return `COPY ${this.source} ${this.dest}`;
+    }
 }
 
 export class Add implements Entry {
-  private readonly source: string;
-  private readonly dest: string;
-  constructor(source: string, dest: string) {
-    this.source = source;
-    this.dest = dest;
-  }
-  render(): string {
-    return `ADD ${this.source} ${this.dest}`;
-  }
+    private readonly source: string;
+    private readonly dest: string;
+
+    constructor(source: string, dest: string) {
+        this.source = source;
+        this.dest = dest;
+    }
+
+    render(): string {
+        return `ADD ${this.source} ${this.dest}`;
+    }
 }
 
 export class Run implements Entry {
-  private readonly command: string;
-  constructor(command: string) {
-    this.command = command;
-  }
-  render(): string {
-    return `RUN ${this.command}`;
-  }
+    private readonly command: string;
+
+    constructor(command: string) {
+        this.command = command;
+    }
+
+    render(): string {
+        return `RUN ${this.command}`;
+    }
 }
 
 export class Container {
-  protected entries: Entry[] = [];
-  public readonly base: Base;
-  constructor(base: Base) {
-    this.base = base;
-    this.entries.push(base);
-  }
-  add(entry: Entry): Container {
-    this.entries.push(entry);
-    return this;
-  }
+    public readonly base: Base;
 
-  render(): string {
-    const manifest = this.entries.map((x) => x.render());
-    return manifest.join("\n");
-  }
+    constructor(base: Base) {
+        this.base = base;
+        this._entries.push(base);
+    }
+
+    private _entries: Entry[] = [];
+
+    get entries(): Entry[] {
+        return this._entries;
+    }
+
+    cmd(...args: string[]): Container {
+        const command = new Command()
+        args.forEach(x => command.add(x))
+        this._entries.push(command)
+        return this;
+    }
+
+    run(data: string): Container {
+        this._entries.push(new Run(data))
+        return this;
+    }
+
+    add(source: string, dest: string): Container {
+        this._entries.push(new Add(source, dest))
+        return this;
+    }
+
+    copy(source: string, dest: string): Container {
+        this._entries.push(new Copy(source, dest))
+        return this;
+    }
+
+    user(data: string): Container {
+        this._entries.push(new User(data))
+        return this;
+    }
+
+    workdir(data: string): Container {
+        this._entries.push(new Workdir(data))
+        return this;
+    }
+
+    port(data: number): Container {
+        this._entries.push(new Port(data))
+        return this;
+    }
+
+    render(): string {
+        const manifest = this._entries.map((x) => x.render());
+        return manifest.join("\n");
+    }
 }
 
 export class Composer {
-  private services: Service[] = [];
+    private services: Service[] = [];
 
-  constructor() {}
-  add(service: Service) {
-    this.services.push(service);
-  }
+    constructor() {
+    }
+
+    add(service: Service) {
+        this.services.push(service);
+    }
 }
 
 export class Service {
-  private readonly name: string;
-  private readonly container: Container;
-  private readonly ports: [Port, Port][] = [];
-  constructor(name: string, container: Container) {
-    this.name = name;
-    this.container = container;
-  }
-  addPorts(a: Port, b: Port) {
-    this.ports.push([a, b]);
-  }
-  addPort(p: Port) {
-    this.ports.push([p, p]);
-  }
-  render(): string {
-    const ports = this.ports
-      .map((x) => `              - "${x[0].port}":"${x[1].port}"`)
-      .join("\n");
-    return `    ${this.name}:
+    private readonly name: string;
+    private readonly container: Container;
+    private readonly ports: [Port, Port][] = [];
+
+    constructor(name: string, container: Container) {
+        this.name = name;
+        this.container = container;
+    }
+
+    addPorts(a: Port, b: Port) {
+        this.ports.push([a, b]);
+    }
+
+    addPort(p: Port) {
+        this.ports.push([p, p]);
+    }
+
+    render(): string {
+        const ports = this.ports
+            .map((x) => `              - "${x[0].port}":"${x[1].port}"`)
+            .join("\n");
+        return `    ${this.name}:
           image: ${this.container.base.from}:${this.container.base.tag}
           ports:
 ${ports}
   `;
-  }
+    }
 }
+
--- docker/build.ts
+++ docker/build.ts
@@ -4,21 +4,13 @@ const base = new c.Base("hayd/alpine-deno", "1.8.1");
 
 const container = new c.Container(base);
 
-container.add(new c.Port(1993));
-container.add(new c.Workdir("/app"));
-container.add(new c.User("deno"));
-container.add(new c.Copy("example.ts", "."));
-container.add(new c.Run("deno cache example.ts"));
-container.add(new c.Add(".", "."));
-container.add(new c.Run("deno cache example.ts"));
-container.add(new c.Command().add("run").add("-A").add("example.ts"));
+container.port(1993)
+    .workdir("/app")
+    .user("deno")
+    .copy("example.js", ".")
+    .run("deno cache example.js")
+    .add(".", ".")
+    .run("deno cache example.js")
+    .cmd("run", "-A", "--unstable", "example.js")
 
 console.log(container.render());
-
-console.log("--------------------------------");
-const service = new c.Service("example", container);
-const p = new c.Port(2181);
-const p2 = new c.Port(2182);
-service.addPorts(p, p);
-service.addPort(p2);
-console.log(service.render());
