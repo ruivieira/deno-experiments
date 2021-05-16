a795c4b4b731bd80064629f6ba47d5a9ee824125
Rui Vieira <ruidevieira@googlemail.com>
2021-05-16 21:27:57 +0100

    Add sysadmin area
    Add plist creator

---
+++ sysadmin/prepare_backup.ts
@@ -0,0 +1,12 @@
+import { exec } from "https://deno.land/x/exec/mod.ts";
+import * as Colors from "https://deno.land/std/fmt/colors.ts";
+
+const HOME = Deno.env.get("HOME")
+
+console.log(Colors.yellow("Sync code folders"));
+let response = await exec(`zsh -c "rsync -avh --exclude-from='${HOME}/Sync/.stignore' --delete ~/Sync/code ~/Dropbox/code/backup/"`);
+console.log(Colors.green("Done!"));
+
+console.log(Colors.yellow("Sync notes folders"));
+response = await exec(`zsh -c "rsync -avh --exclude-from='${HOME}/Sync/.stignore' --delete ~/notes ~/Dropbox/notes/"`);
+console.log(Colors.green("Done!"));
