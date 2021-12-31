979dab49c224a2074a06d9ec9bca73cd6e7eebf3
Rui Vieira <ruidevieira@googlemail.com>
2021-12-31 17:56:15 +0000

    Add Joplin support for full notes

--- notes/joplin.ts
+++ notes/joplin.ts
@@ -4,6 +4,14 @@
 
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
+export interface Note {
+  id: number;
+  title: string;
+  body: string;
+  createdTime: Date;
+}
+
+
 const HOME = Deno.env.get("HOME");
 
 export function getJoplinDB(): DB {
@@ -13,7 +21,21 @@ export function getJoplinDB(): DB {
 export function listTitles(db: DB): Array<string> {
   const titles: Array<string> = [];
   for (const [title] of db.query("SELECT title FROM notes")) {
-    titles.push(title);
+    
+    titles.push(title as string);
   }
   return titles;
+}
+
+export function getAllNotes(db: DB): Array<Note> {
+  const notes: Array<Note> = []; 
+  for (const [id, title, body, createdTime] of db.query("SELECT id, title, body, created_time FROM notes")) {
+    notes.push({
+      id: id as number,
+      title: title as string,
+      body: body as string,
+      createdTime: new Date(createdTime as number)
+    });
+  }
+  return notes;
 }
