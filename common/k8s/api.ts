f7ac3205c641d5e694c776eeb6e5f6418dec6461
Rui Vieira <ruidevieira@googlemail.com>
2022-02-19 11:44:15 +0000

    Add K8s deployment manager

--- common/k8s/api.ts
+++ common/k8s/api.ts
@@ -1,12 +1,57 @@
 /**
  * INFO: Interface definitions for K8s resources
  */
-export interface Deployment extends Record<string, unknown> {
-    apiVersion: string,
+
+import { stringify as yamlStringify } from "https://deno.land/std/encoding/yaml.ts";
+
+export interface KubernetesResource extends Record<string, unknown> {
+  apiVersion: string;
+  kind: string;
+}
+
+export interface Deployment extends KubernetesResource {
+  metadata: {
+    name: string;
+    labels: {
+      app: string;
+    };
+  };
+}
+
+const defaultDeployment = (): Deployment => {
+  const d = {
+    apiVersion: "app/v1",
+    kind: "Deployment",
     metadata: {
-        name: string,
-        labels: {
-            app: string
-        }
-    }
+      name: "octopus-deployment",
+      labels: {
+        app: "web",
+      },
+    },
+  };
+  return d;
+};
+
+interface ResourceManager {
+  asYaml(): string;
+}
+
+abstract class AbstractResourceManager<T extends Record<string, unknown>>
+  implements ResourceManager
+{
+  readonly resource: T;
+
+  constructor(resource: T) {
+    this.resource = resource;
+  }
+
+  asYaml(): string {
+    return yamlStringify(this.resource);
+  }
+}
+
+export class DeploymentManager extends AbstractResourceManager<Deployment> {
+  constructor() {
+    super(defaultDeployment());
+  }
 }
--- k8s/api.ts
+++ k8s/api.ts
@@ -1,23 +1,7 @@
-import {
-stringify as yamlStringify,
-  } from 'https://deno.land/std/encoding/yaml.ts';
-import { Deployment } from "../common/k8s/api.ts";
+import { DeploymentManager } from "../common/k8s/api.ts";
 
+const d = new DeploymentManager();
 
+d.resource.metadata.name = "test";
 
-const test: Deployment = {
-    apiVersion: "app/v1",
-    metadata: {
-        name: "octopus-deployment",
-        labels: {
-            app: "web"
-        }
-    }
-}
-
-export const getDeploymentYaml = (deployment: Deployment) : string => {
-    deployment["kind"] = "Deployment"
-    return yamlStringify(test)
-}
-
-console.log(getDeploymentYaml(test))
+console.log(d.asYaml());
